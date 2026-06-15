58478ff67424ddd26ea09aedaca2144ebd593fe0
Harsh Shinde <therealharshshinde@gmail.com>
2026-06-15 09:38:19 -0400

    created intelligence tree but did not finish yet

--- Subs/Intelligence.py
+++ Subs/Intelligence.py
@@ -1,4 +1,69 @@
-from Sub import * 
+import sys
+from pathlib import Path
+sys.path.append(str(Path(__file__).resolve().parent.parent))
+from Skill import Skill
+from Sub import Sub
 
     
 Intelligence = Sub("Intelligence", 3)
+
+
+# ROW 1
+
+ROAD_WARRIOR = Skill("ROAD WARRIOR", Intelligence, "Allows you to use Sandevistan to slow time while driving. Allows Kerenzikov to be activated when aiming and handbraking simultaneously. +25% weapon damage when your vehicle is drifting or airborne.")
+EYE_IN_THE_SKY = Skill("EYE IN THE SKY", Intelligence)
+FORCEKILL_CYPHER = Skill("Forcekill Cypher", Intelligence)
+
+WARNING_EXPLOSION_HAZAD = Skill("WARNING: EXPLOSION HAZARD", Intelligence)
+OPTIMIZATION = Skill("OPTIMIZATION", Intelligence)
+PROXIMATE_PROPAGATION = Skill("PROXIMATE PROPAGATION", Intelligence)
+ENCRYPTION = Skill("ENCRYPTION", Intelligence)
+SUBORDINATION = Skill("SUBORDINATION", Intelligence)
+
+CARHACKER = Skill("CARHACKER", Intelligence)
+
+
+
+# ROW 2
+
+HACK_QUEUE = Skill("HACK QUEUE", Intelligence)
+FEEDBACK_LOOP = Skill("FEEDBACK LOOP", Intelligence)
+DATA_RECYCLER = Skill("DATA RECYCLER", Intelligence)
+COPY_PASTE = Skill("COPY-PASTE", Intelligence)
+COUNTER_A_HACK = Skill("COUNTER-A-HACK", Intelligence)
+
+EMBEDDED_EXPLOIT = Skill("EMBEDDED EXPLOIT", Intelligence)
+SHADOWRUNNER =  Skill("SHADOWRUNNER", Intelligence)
+ICEPICK = Skill("ICEPICK", Intelligence)
+SYSTEM_OVERWHELM = Skill("SYSTEM OVERHWELM", Intelligence)
+SPECULATION = Skill("SPECULATION", Intelligence)
+
+SIPHON = Skill("SIPHON", Intelligence)
+RECIRCULATION = Skill("RECIRCULATION", Intelligence)
+
+ACQUISITION_SPECIALIST = Skill("ACQUISITION SPECIALIST", Intelligence)
+PRECISION_SUBROUTINES = Skill("PRECISIION SUBROUTINES", Intelligence)
+NO_ESCAPE = Skill("NO ESCAPE", Intelligence)
+
+# ROW 3
+QUEUE_ACCELERATION = Skill("QUEUE-ACCELERATION", Intelligence)
+FINISHER_LIVE_WIRE = Skill("FINISHER LIVE WIRE", Intelligence) 
+QUEUE_PRIORITIZATION = Skill("QUEUE PRIORITIATION", Intelligence)
+QUEUE_HACK_ROOT = Skill("QUEUE HACK_ROOT", Intelligence)
+
+
+BLOOD_DAEMON = Skill("BLOOD DAEMON", Intelligence)
+
+OVERCLOCK = Skill("OVERCLOCK", Intelligence)
+SUBLIMATION = Skill("SUBLIMATION", Intelligence)
+POWER_SURGE = Skill("POWER SURGE", Intelligence)
+RACE_AGAINST_MIND = Skill("RACE AGAINST MIND", Intelligence)
+
+TARGET_LOCK_TRANSFER = Skill("TARGET LOCK TRANSFER", Intelligence)
+TERMINAL_VELOCITY = Skill("TERMINAL VELOCITY", Intelligence)
+TARGETING_PRISM = Skill("TARGETING PRISM", Intelligence)
+# ROW 4
+
+QUEUE_MASTERY = Skill("QUEUE MASTERY", Intelligence)
+SPILLOVER = Skill("SPILLOVER", Intelligence)
+SINERGY = Skill("SINERGY", Intelligence)
--- Subs/Reflexes.py
+++ Subs/Reflexes.py
@@ -1,4 +1,8 @@
-from Sub import *
+import sys
+from pathlib import Path
+sys.path.append(str(Path(__file__).resolve().parent.parent))
+from Skill import Skill
+from Sub import Sub
 
  
 Reflexes = Sub("Reflexes", 3)
